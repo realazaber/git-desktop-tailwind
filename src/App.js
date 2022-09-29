97cdc1755f44c3370090a4e210c78a4347142700
swagoverlord <azcooldude8@gmail.com>
2022-09-30 01:16:52 +0200

    Applied background colour

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./styles/output.css";
 
 function App() {
-  return <div className="App">
+  return <div className="h-15 bg-gradient-to-b from-gray-900 to-gray-800">
     <h1>
       Hello world
+     
     </h1>
   </div>;
 }
