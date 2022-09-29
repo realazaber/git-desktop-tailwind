0675f24774f25a6787f72cc4e8fe72daba5294a5
swagoverlord <azcooldude8@gmail.com>
2022-09-30 00:41:45 +0200

    Setup Tailwind
    Deleted unneccessary files and configured tailwind to work.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './public/**/*.html',
+    './public/**/*.js'
+  ],
   theme: {
     extend: {},
   },
