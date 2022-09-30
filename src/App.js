364f42e1ae864775e1b7169683079817e77629b2
swagoverlord <azcooldude8@gmail.com>
2022-09-30 02:31:22 +0200

    Reorganised files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles/output.css";
-import Header from "./components/header";
+import Header from "./components/sections/header";
+import Hero from "./components/sections/hero";
 
 function App() {
   return <div className="h-15 bg-gradient-to-b from-gray-900 to-gray-800">
