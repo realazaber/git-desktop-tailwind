5c64f79d7ff399fd0203c5a2911a1110c224cb2d
swagoverlord <azcooldude8@gmail.com>
2022-09-30 03:26:26 +0200

    imported bg img

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/sections/header";
 import Hero from "./components/sections/hero";
 
 function App() {
-  return <div className="h-15 bg-gradient-to-b from-gray-900 to-gray-800">
+  return <div className="h-15 bg-gradient-to-b from-gray-900 to-gray-800]">
     <Header />
   </div>;
 }
