8f09f5075d2b92e995f17bb244f13a82a7564146
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2022-10-15 08:30:07 +0200

    Card Group takes in components

---
+++ src/components/Cards/CardGroup.jsx
@@ -0,0 +1,9 @@
+const CardGroup = ({ children }) => {
+  return (
+    <div className="flex align-middle justify-center my-2 w-[900px] m-auto">
+      {children}
+    </div>
+  );
+};
+
+export default CardGroup;
