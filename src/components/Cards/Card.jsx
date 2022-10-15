d23d3f7f7ddd8c3aeb15c20a9ac2d06eb37332e1
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2022-10-15 09:03:51 +0200

    Added more props for card
    Props:
    Title
    bodyStart
    downloadLink
    downloadText
    bodyEnd

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,8 +1,14 @@
 const Card = (props) => (
-  <div className="flex flex-col">
+  <div className="flex flex-col w-4/12 text-center">
     {/* <img src={props.img} alt={props.title} /> */}
-    <h3 className="text-white">{props.title}</h3>
-    <p className="text-slate-600">{props.body}</p>
+    <h3 className="text-white my-2 font-semibold">{props.title}</h3>
+    <p className="text-gray-400">
+      {props.bodyStart}{" "}
+      <a className="text-purple-500" href={props.downloadLink}>
+        {props.downloadText}
+      </a>
+      {props.bodyEnd}
+    </p>
   </div>
 );
 
