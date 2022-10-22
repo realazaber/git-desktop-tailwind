ddaac6f6b465392809c818b2a94e8633c6b14e80
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2022-10-22 23:16:23 +0200

    Each card is same height

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -3,14 +3,18 @@ import { useState } from "react";
 function Card(props) {
   let imgUrl = "/images/" + props.img;
   return (
-    <div className="flex flex-col w-4/12 text-center mx-1">
+    <div className="flex flex-col w-4/12 text-center mx-1 h-48">
       {imgUrl != "/images/" ? (
-        <img src={imgUrl} alt={props.title} className="w-8 m-auto" />
+        <img
+          src={imgUrl}
+          alt={props.title}
+          className="w-14 m-auto bg-[#7b63ab] py-3 px-4 rounded"
+        />
       ) : (
         ""
       )}
       <h3 className="text-white my-2 font-semibold">{props.title}</h3>
-      <p className="text-gray-400 font-normal">
+      <p className="text-gray-400 font-normal h-24">
         {props.bodyStart}{" "}
         <a className="link" href={props.downloadLink}>
           {props.downloadText}
