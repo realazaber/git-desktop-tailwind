dd7bbb3c60ad9e22832ae2d34ac8b09329d4df16
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2022-10-22 22:21:56 +0200

    Implemented flex reverse option

---
+++ src/components/Cards/FeatureCard.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+function FeatureCard(props) {
+  const [imgPath, setImgPath] = useState("/images/webps/");
+  const [rowClass, setRowClass] = useState(
+    "flex align-middle justify-between px-5"
+  );
+
+  useEffect(() => {
+    if (props.reverse) {
+      setRowClass(rowClass + " flex-row-reverse");
+    } else {
+      setRowClass(rowClass + " flex-row");
+    }
+    setImgPath(imgPath + props.img + ".webp");
+  }, []);
+
+  return (
+    <div className={rowClass}>
+      <div className="w-1/2 flex align-middle justify-center">
+        <img src={imgPath} alt={props.title} className="w-2/3 p-5" />
+      </div>
+      <div className="w-1/2 p-5 flex flex-col align-middle justify-center">
+        <h3 className="text-white font-light">{props.title}</h3>
+        <p className="text-slate-400">
+          {props.paragraph}
+          <a
+            href={props.link}
+            target="_blank"
+            rel="noreferrer"
+            className="link"
+          >
+            {props.linkText}
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default FeatureCard;
