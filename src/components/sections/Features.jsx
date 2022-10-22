4232e3f58b3cb77be408a1962262f2679e7805b5
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2022-10-22 22:13:35 +0200

    Added feature cards data

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -1,6 +1,7 @@
 import { getOsName } from "../../helpers/getOS";
 import { useState, useEffect } from "react";
 import FeatureCardGroup from "../Cards/FeatureCardGroup";
+import FeatureCard from "../Cards/FeatureCard";
 
 function Features() {
   const osName = getOsName();
@@ -24,7 +25,36 @@ function Features() {
         src={imgPath}
         alt="Main Screenshot"
       />
-      <FeatureCardGroup />
+      <FeatureCardGroup>
+        <FeatureCard
+          img="co-authoring"
+          title="Attribute commits with collaborators easily"
+          paragraph="Quickly add co-authors to your commit. 
+          Great for pairing and excellent for sending a little 
+          love/credit to that special someone who helped fix 
+          that gnarly bug of yours. See the attribution on
+           the history page, undo an accidental attribution, and"
+          link="https://github.blog/2018-01-29-commit-together-with-co-authors/"
+          linkText=" see the co-authors on github.com."
+        />
+        <FeatureCard
+          img="pr-checks"
+          title="Checkout branches with pull requests and 
+          view CI statuses"
+          paragraph="See all open pull requests for your 
+          repositories and check them out as if they were 
+          a local branch, even if they're from upstream 
+          branches or forks. See which pull requests pass 
+          commit status checks, too!"
+        />
+        <FeatureCard
+          img="syntax-highlighting"
+          title="Syntax highlighted diffs"
+          paragraph="The new GitHub Desktop supports 
+          syntax highlighting when viewing diffs for a 
+          variety of different languages."
+        />
+      </FeatureCardGroup>
     </div>
   );
 }
