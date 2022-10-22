f941ec3d4e752e9d5b236734f97abdfafe9133c3
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2022-10-22 21:17:01 +0200

    Created container for consistent spacing

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./styles/main.css";
 import Header from "./components/Header";
 import Hero from "./components/sections/Hero";
 import Background from "./components/sections/Background";
+import Features from "./components/sections/Features";
 
 const App = () => (
   <div className="h-15 bg-gradient-to-b from-neutral-900 to-neutral-800">
     <Background />
-    <Header />
-    <Hero />
+    <div className="container">
+      <Header />
+      <Hero />
+      <Features />
+    </div>
   </div>
 );
 
